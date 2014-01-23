050ec60205441e30cefa88db27de76df29a7207e
Gordon Potter <eccegordo@gmail.com>
2014-01-22 20:23:23 -0800

    Reset routes and templates to base state for demo
    We want to start with a base demo. Note that there are other parts of the completed app in this branch. Those have no immediate bearing on what we are going to show in this demo and can be ignored. This demo is going to showcase how to use the router and how to create basic page views in ember with templates.

--- app/routes/application.js
+++
@@ -1,7 +0,0 @@
-export default Ember.Route.extend({
-  actions: {
-    transitionToLocationHandler: function (location) {
-      this.transitionTo('weather', location);
-    }
-  }
-});
--- app/routes/weather.js
+++
@@ -1,6 +0,0 @@
-export default Ember.Route.extend({
-
-  model: function(params) {
-    return this.store.find('weather', params.location_id);
-  }
-});
