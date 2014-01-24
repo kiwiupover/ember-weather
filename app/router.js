bae2964d9be22b0134fe98930450e8ab3f09edcc
Gordon Potter <eccegordo@gmail.com>
2014-01-23 17:18:37 -0800

    All the code for the routes demo
    If you get lost look in the folder
    extras/code_snippets.txt
    the code for each file is there

--- app/router.js
+++ app/router.js
@@ -1,11 +1,118 @@
-var Router = Ember.Router.extend(); // ensure we don't share routes between all Router instances
+var Router = Ember.Router.extend(); 
 
 Router.map(function() {
+    this.resource('static', {path: 'static'}, function(){
+        this.route('seattle');
+        this.route('honolulu');
+    });
+
+    this.resource("locations", {path: 'locations'}, function(){
+        this.resource("weather", {path: ':location_id'}, function(){});
+    });
 
 });
 
 Router.reopen({
+  // Define the way urls work and look
   location: 'history'
+  // Supports three types:
+  //
+  // location: 'hash'    => example.com/#/locations/seattle
+  // location: 'history' => example.com/locations/seattle
+  // location: 'none'    => example.com
 });
 
 export default Router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+var Router = Ember.Router.extend(); 
+
+Router.map(function() {
+    this.resource('static', {path: 'static'}, function(){
+        this.route('seattle');
+        this.route('honolulu');
+    });
+
+    this.resource("locations", {path: 'locations'}, function(){
+        this.resource("weather", {path: ':location_id'}, function(){});
+    });
+
+});
+
+Router.reopen({
+  // Define the way urls work and look
+  location: 'history'
+  // Supports three types:
+  //
+  // location: 'hash'    => example.com/#/locations/seattle
+  // location: 'history' => example.com/locations/seattle
+  // location: 'none'    => example.com
+});
+
+export default Router;
+
+*/
---
+++ app/routes/weather.js
@@ -0,0 +1,85 @@
+export default Ember.Route.extend({
+    model: function(params) {
+        return params.location_id;
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+export default Ember.Route.extend({
+    model: function(params) {
+        return params.location_id;
+    }
+});
+
+*/
