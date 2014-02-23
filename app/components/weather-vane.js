1581027b1bcdd9022e8e0b52598f2139765cd1ea
Dave Laird <kiwiupover@gmail.com>
2014-02-22 16:44:40 -0800

    Added a better weather vane

--- app/components/weather-vane.js
+++ app/components/weather-vane.js
@@ -1,22 +1,10 @@
 export default Ember.Component.extend({
   windDegrees: 0,
-  // tagName: 'i',
-  // classNames: 'icon-weathervane',
+  attributeBindings: 'style',
 
-  // attributeBindings: 'style',
-
-  didInsertElement: function(){
-    var degrees = this.get('windDegrees'),
-        cssRotation = "rotate(" + degrees + "deg)";
-        // "-webkit-transform: rotate(" + degrees +"deg), " +
-        //               "transform: rotate(" + degrees +"deg)";
-    window.console.log("cssRotation: " + cssRotation);
-    return this.$('.circle').css({'-webkit-transform': cssRotation, transform: cssRotation});
+  style: function(){
+    var degrees = this.get('windDegrees');
+    return "-webkit-transform: rotate(" + degrees +"deg); transform: rotate(" + degrees +"deg);";
   }.property('windDegrees')
-
-  // style: function(){
-  //   var degrees = this.get('windDegrees') - 0;
-  //   return "-webkit-transform: rotate(" + degrees +"deg)";
-  // }.property('windDegrees')
 
 });
