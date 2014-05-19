e057dce7de633d092532d4a9b8e3bdc9d2bd4ab9
Dave Laird <kiwiupover@gmail.com>
2014-05-18 22:16:33 -0700

    added stuff

--- app/routes/playground.js
+++ app/routes/playground.js
@@ -1,4 +1,4 @@
-var PlayGround = Ember.Route.extend({
+var Playground = Ember.Route.extend({
 
   model: function(){
     return this.store.find('playground');
@@ -6,7 +6,81 @@ var PlayGround = Ember.Route.extend({
 
 });
 
-export default PlayGround;
+
+
+Playground.FIXTURES = [
+  {
+    id: 1,
+    date: "Sat Jun 01 2013 00:00:00 GMT-0700 (PDT)",
+    high: 64,
+    low: 44,
+    label: 'San Francisco'
+  },
+  {
+    id: 2,
+    date: "Sat Jun 01 2013 00:00:00 GMT-0700 (PDT)",
+    high: 93,
+    low: 83,
+    label: 'Phoenix'
+  },
+  {
+    id: 3,
+    date: "Mon Jul 01 2013 00:00:00 GMT-0700 (PDT)",
+    high: 66,
+    low: 46,
+    label: 'San Francisco'
+  },
+  {
+    id: 4,
+    date: "Mon Jul 01 2013 00:00:00 GMT-0700 (PDT)",
+    high: 103,
+    low: 93,
+    label: 'Phoenix'
+  }
+];
+
+export default Playground;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
