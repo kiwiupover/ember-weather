1404f2cb8664dae3800dfcc60c7d57c5a833bd76
Dave Laird <kiwiupover@gmail.com>
2014-05-21 15:57:40 -0700

    removed extra files

--- app/adapters/weather.js
+++
@@ -1,8 +0,0 @@
-/*global localStorage*/
-import getJSON from "appkit/utils/get-json";
-
-export default DS.Adapter.extend({
-  find: function (store, type, id) {
-    return getJSON('/api/weather/' + id);
-  }
-});
--- app/components/weather-current-details.js
+++
@@ -1,8 +0,0 @@
-export default Ember.Component.extend({
-
-  displayDate: function () {
-    return moment.unix(this.get('weather.time')).format('MMM DD');
-  }.property('weather.time')
-
-
-});
