de55d82181639364b7cf3534b11789f415d76a2d
Dave Laird <kiwiupover@gmail.com>
2016-09-29 10:34:28 -0700

    added npm and bower modules

--- app/app.js
+++ app/app.js
@@ -1,15 +1,18 @@
 import Ember from 'ember';
-import Resolver from 'ember/resolver';
-import loadInitializers from 'ember/load-initializers';
+import Resolver from './resolver';
+import loadInitializers from 'ember-load-initializers';
+import config from './config/environment';
+
+let App;
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
-var App = Ember.Application.extend({
-  modulePrefix: 'ember-weather', // TODO: loaded via config
-  rootElement: '#main-content',
-  Resolver: Resolver
+App = Ember.Application.extend({
+  modulePrefix: config.modulePrefix,
+  podModulePrefix: config.podModulePrefix,
+  Resolver
 });
 
-loadInitializers(App, 'ember-weather');
+loadInitializers(App, config.modulePrefix);
 
 export default App;
